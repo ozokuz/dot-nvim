78599d846130fcceb596a636b773b453c34f1d34
Ozoku <19756856+ozokuz@users.noreply.github.com>
2023-06-06 00:45:41 +0300

    git workflows

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local Util = require("lazyvim.util")
+
+local function map(mode, lhs, rhs, desc)
+  vim.keymap.set(mode, lhs, rhs, { noremap = true, silent = true, desc = desc })
+end
+
+map("n", "<leader>gl", function()
+  Util.float_term({ "lazygit" }, { cwd = Util.get_root(), esc_esc = false })
+end, "Lazygit (root dir)")
---
+++ lua/plugins/git.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "sindrets/diffview.nvim",
+    keys = { { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Git: Diff" } },
+  },
+  {
+    "TimUntersberger/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      integrations = { diffview = true },
+    },
+    keys = { { "<leader>gg", "<cmd>Neogit<cr>", desc = "Git: Status" } },
+  },
+  {
+    "tpope/vim-fugitive",
+    lazy = false,
+  },
+  {
+    "pwntester/octo.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    cmd = "Octo",
+    config = true,
+  },
+}
