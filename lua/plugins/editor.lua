9a47926d793789d161d346781acb009040ba0f63
Ozoku <19756856+ozokuz@users.noreply.github.com>
2023-06-07 19:34:46 +0300

    markdown preview

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -70,4 +70,23 @@ return {
       })
     end,
   },
+  {
+    "toppair/peek.nvim",
+    build = "deno task --quiet build:fast",
+    keys = {
+      {
+        "<leader>mp",
+        function()
+          local peek = require("peek")
+          if peek.is_open() then
+            peek.close()
+          else
+            peek.open()
+          end
+        end,
+        desc = "Markdown Preview",
+      },
+    },
+    config = true,
+  },
 }
