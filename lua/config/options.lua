79ffec80d97891b761b5a1c67da47209600d0137
Ozoku <19756856+ozokuz@users.noreply.github.com>
2023-06-07 20:20:09 +0300

    finish statuscolumn settings

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -16,3 +16,4 @@ vim.opt.fillchars = {
 vim.o.foldlevel = 99
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
+vim.o.foldcolumn = "1"
--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -57,14 +57,18 @@ return {
         bt_ignore = { "terminal", "nofile" },
         relculright = true,
         segments = {
-          { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
-          { sign = { name = { "Diagnostic" }, maxwidth = 2, auto = true }, click = "v:lua.ScSa" },
+          {
+            sign = { name = { ".*" }, maxwidth = 1 },
+            click = "v:lua.ScSa",
+          },
+          { sign = { name = { "Diagnostic" }, maxwidth = 1, auto = true }, click = "v:lua.ScSa" },
           { text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
           { text = { " " } },
           {
-            sign = { name = { "GitSigns" }, maxwidth = 2, colwidth = 1, wrap = true, auto = true },
+            sign = { name = { "GitSigns" }, maxwidth = 1, auto = true },
             click = "v:lua.ScSa",
           },
+          { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
           { text = { " " } },
         },
       })
