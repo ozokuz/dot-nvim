5ab02333cd1b181e6c565960a7fb3a1ac0e3cff5
Ozoku <19756856+ozokuz@users.noreply.github.com>
2023-06-28 14:08:24 +0300

    remove lsp hover no info available notifications

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -41,4 +41,16 @@ return {
       }
     end,
   },
+  {
+    "folke/noice.nvim",
+    opts = function(_, opts)
+      table.insert(opts.routes, {
+        filter = {
+          event = "notify",
+          find = "No information available",
+        },
+        opts = { skip = true },
+      })
+    end,
+  },
 }
