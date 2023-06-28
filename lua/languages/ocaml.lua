06e21c9ace502133ede2244e146cf7126bddb67c
Ozoku <19756856+ozokuz@users.noreply.github.com>
2023-06-28 12:05:15 +0300

    ocaml setup

---
+++ lua/languages/ocaml.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        ocamllsp = {},
+      },
+    },
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "ocaml-lsp", "ocamlformat" })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "ocaml", "ocaml_interface" })
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      local null_ls = require("null-ls")
+      table.insert(opts.sources, null_ls.builtins.formatting.ocamlformat)
+    end,
+  },
+}
