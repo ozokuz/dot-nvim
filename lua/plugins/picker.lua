3b4930f9e5c469e5559fa4ee450ca71cf143083e
Ozoku <19756856+ozokuz@users.noreply.github.com>
2023-10-11 21:58:43 +0300

    telescope fzf native is now included by default

--- lua/plugins/picker.lua
+++ lua/plugins/picker.lua
@@ -1,12 +1,6 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
-    dependencies = {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-      config = function()
-        require("telescope").load_extension("fzf")
-      end,
     },
   },
 }
