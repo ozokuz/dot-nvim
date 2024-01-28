0f92681edeac4e8634b19d2ae427c8e36dd2d41f
Ozoku <19756856+ozokuz@users.noreply.github.com>
2024-01-28 18:44:26 +0200

    open oil if opening nvim with a directory

--- lua/plugins/files.lua
+++ lua/plugins/files.lua
@@ -8,6 +8,14 @@ return {
   },
   {
     "stevearc/oil.nvim",
+    init = function()
+      if vim.fn.argc(-1) == 1 then
+        local stat = vim.loop.fs_stat(vim.fn.argv(0))
+        if stat and stat.type == "directory" then
+          require("oil")
+        end
+      end
+    end,
     opts = {},
     dependencies = { "nvim-tree/nvim-web-devicons" },
     keys = {
