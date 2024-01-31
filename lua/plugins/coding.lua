a7a35fa53c37c0bb41a31caa3d5df3f338479e87
Ozoku <19756856+ozokuz@users.noreply.github.com>
2024-01-31 21:20:32 +0200

    fix redo not being usable

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -120,7 +120,7 @@ return {
       vim.keymap.set("n", "o", "o<cmd>AutolistNewBullet<cr>")
       vim.keymap.set("n", "O", "O<cmd>AutolistNewBulletBefore<cr>")
       vim.keymap.set("n", "<CR>", "<cmd>AutolistToggleCheckbox<cr><CR>")
-      vim.keymap.set("n", "<C-r>", "<cmd>AutolistRecalculate<cr>")
+      vim.keymap.set("n", "<M-r>", "<cmd>AutolistRecalculate<cr>")
 
       vim.keymap.set("n", "<leader>cn", require("autolist").cycle_next_dr, { expr = true })
       vim.keymap.set("n", "<leader>cp", require("autolist").cycle_prev_dr, { expr = true })
